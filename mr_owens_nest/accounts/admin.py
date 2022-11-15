e358812fecb945ee2c46bbb095d93eb18b7415bc
Jishnu Prasad Samal <jishnuprs20@gmail.com>
2022-11-15 23:32:02 +0530

    added user model to admin site

--- mr_owens_nest/accounts/admin.py
+++ mr_owens_nest/accounts/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+# @admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'first_name', 'last_name', 'email', 'date_joined']
+    readonly_fields = ['date_joined']
+    list_display_links = ['username']
+    search_fields = ['first_name', 'last_name', 'email', 'username']
+    ordering = ['-date_joined']
+    fieldsets = (
+        (None, {
+            'fields': ('first_name', 'last_name',)
+        }),
+        ('Login Details', {
+            'fields': ('email', 'username', 'date_joined')
+        }),
+    )
+
+admin.site.register(User, UserAdmin)
--- mr_owens_nest/accounts/models.py
+++ mr_owens_nest/accounts/models.py
@@ -2,11 +2,15 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    """
+    :model: `accounts.User`
+    Custom User Model
+    
+    """
     username = models.CharField(max_length=255, unique=True)
     email = models.EmailField(('email address'), unique=True)
     first_name = models.CharField(('first name'), max_length=30, blank=True)
     last_name = models.CharField(('last name'), max_length=30, blank=True)
-    # password = models.Pa
     date_joined = models.DateTimeField(('date joined'), auto_now_add=True)
     is_active = models.BooleanField(('active'), default=True)
     
@@ -14,5 +18,5 @@ class User(AbstractUser):
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
     
     class Meta:
-        verbose_name = ('User')
-        verbose_name_plural = ('Users')
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
